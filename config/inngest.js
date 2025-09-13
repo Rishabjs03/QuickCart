26c13a2f8a06a2ee87bb7293b1796a60a65b3743
Rishabjs03 <rishabagarwaljs@gmail.com>
2025-09-13 20:13:42 +0530

    a bug

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDb from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -47,5 +48,30 @@ export const deleteUser = inngest.createFunction(
     const { id } = event.data;
     await connectDb();
     await User.findByIdAndDelete(id);
+  }
+);
+// inngest function to create user order
+export const createUserOrder = inngest.createFunction(
+  {
+    id: "create-user-order",
+    batchEvents: {
+      maxSize: 5,
+      timeout: "5s",
+    },
+  },
+  { event: "order/created" },
+  async ({ events }) => {
+    const orders = events.map((event) => {
+      return {
+        userId: event.data.user,
+        items: event.data.items,
+        amount: event.data.amount,
+        address: event.data.address,
+        date: event.data.date,
+      };
+    });
+    await connectDb();
+    await Order.insertMany(orders);
+    return { success: true, processed: orders.length };
   }
 );
